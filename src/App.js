0c573bfb1b1eada1ba4fa1eed7bfd9c4df3ee97c
kimminkyu <kmk50009@naver.com>
2022-02-07 18:34:13 +0900

    Add Dynamic Path

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import MainHeader from "./components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <main>
         <Routes>
           <Route path="/welcome" element={<Welcome />} />
-          <Route path="/products" element={<Products />} />
+          <Route path="/products" element={<Products />} exact/>
+          <Route path="/products/:productId" element={<ProductDetail />} />
         </Routes>
       </main>
     </div>
