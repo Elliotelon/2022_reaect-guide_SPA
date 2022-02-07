74cbc0eb3aff9100bcd27ae459f505c529d7c15d
kimminkyu <kmk50009@naver.com>
2022-02-07 13:50:32 +0900

    Add NavLink

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,15 +1,26 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import classes from "./MainHeader.module.css";
 
 const MainHeader = () => {
   return (
-    <header>
+    <header className={classes.header}>
       <nav>
         <ul>
           <li>
-            <Link to="/welcome">Welcome</Link>
+            <NavLink
+              className={({ isActive }) => (isActive ? classes.active : "")}
+              to="/welcome"
+            >
+              Welcome
+            </NavLink>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <NavLink
+              className={({ isActive }) => (isActive ? classes.active : "")}
+              to="/products"
+            >
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
