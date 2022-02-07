72f7510cf94c9fd552e70f2cd7fe8ae0fbfc60d4
kimminkyu <kmk50009@naver.com>
2022-02-07 13:16:36 +0900

    Add Path Definition

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Routes>
+        <Route path="/welcome" element={<Welcome />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+// our-domain.com/ => Component A
+// our-domain.com/products => Component B
