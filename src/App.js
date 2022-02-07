399932b5cf050b237aa08d4f064dc32c5afd0a1c
kimminkyu <kmk50009@naver.com>
2022-02-07 19:43:38 +0900

    Add Navigate

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import MainHeader from "./components/MainHeader";
@@ -10,8 +10,9 @@ function App() {
       <MainHeader />
       <main>
         <Routes>
+          <Route path="/" element={<Navigate to="/welcome"/>} />
           <Route path="/welcome/*" element={<Welcome />} /> {/**Nested Routes v6 */}
-          <Route path="/products" element={<Products />} exact/>
+          <Route path="/products" element={<Products />} />
           <Route path="/products/:productId" element={<ProductDetail />} />
         </Routes>
       </main>
